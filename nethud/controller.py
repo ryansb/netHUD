f0ccff961b79cb0403419ef97277e3a842e37b8d
Nathaniel Case <Qalthos@gmail.com>
2013-01-21 12:43:11 -0500

    Container for map display data, with easy updating.

--- nethud/controller.py
+++ nethud/controller.py
@@ -4,6 +4,64 @@ except ImportError:
     import json
 
 
+class LevelDetails(object):
+    display = {}
+
+    def update(self, display):
+        for obj in display:
+            try:
+                key = obj.keys()[0]
+                getattr(self, key)(obj[key])
+            except:
+                pass
+
+    def get(self, key):
+        return self.display.get(key, [])
+
+    def list_items(self, items):
+        if items['invent'] == 1:
+            display['list_items'] = items['items']
+
+    def update_screen(self, mapdelta):
+        try:
+            details = self.display['update_screen']
+        except:
+            self.display['update_screen'] = mapdelta['dbuf']
+            return
+
+        for col_index, col in enumerate(mapdelta):
+            # 0: this column is empty; clear
+            if col == 0:
+                details[col_index] = 0
+            # 1: nothing has changed since last update; ignore
+            elif col == 1:
+                continue
+            # list: something has changed
+            elif isinstance(col, list):
+                # If there isn't a list here already, the whole thing is new
+                if not isinstance(details[col_index], list):
+                    details[col_index] = col
+                    continue
+                for row_index, cell in enumerate(col):
+                    if cell == 0 or isinstance(cell, list):
+                        # I think we can just drop it in, no need to check for
+                        # 1s
+                        details[col_index][row_index] = 0
+
+        self.display['update_screen'] = details
+
+    def update_status(self, statusdelta):
+        try:
+            status = self.display['update_status']
+        except:
+            self.display['update_status'] = statusdelta
+            return
+
+        for key in statusdelta.keys():
+            status[key] = statusdelta[key]
+        self.display['update_status'] = status
+
+
 class Controller(object):
     users = {}
     cached_details = {}
